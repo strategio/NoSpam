9dd945a8c098015d0d772f8da57be722d97d79ea
STRATEGIO <strategio@strategio.fr>
2013-08-13 18:53:44 -0300

    First commit

---
+++ views/admin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Represents the view for the administration dashboard.
+ *
+ * This includes the header, options, and other information that should provide
+ * The User Interface to the end user.
+ *
+ * @package   No_Spam
+ * @author    Pierre SYLVESTRE <strategio@strategio.fr>
+ * @license   GPL-2.0+
+ * @link      http://www.strategio.fr
+ * @copyright 2013 Strategio
+ */
+?>
+<div class="wrap">
+
+	<?php screen_icon(); ?>
+	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+
+	<!-- TODO: Provide markup for your options page here. -->
+
+</div>
---
+++ views/public.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Represents the view for the public-facing component of the plugin.
+ *
+ * This typically includes any information, if any, that is rendered to the
+ * frontend of the theme when the plugin is activated.
+ *
+ * @package   No_Spam
+ * @author    Pierre SYLVESTRE <strategio@strategio.fr>
+ * @license   GPL-2.0+
+ * @link      http://www.strategio.fr
+ * @copyright 2013 Strategio
+ */
+?>
+<!-- This file is used to markup the public facing aspect of the plugin. -->
